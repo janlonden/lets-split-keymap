74b7c49fe81302ec057ca8e35eb4733bd991d535
Jan Londén <jjlonden@gmail.com>
2017-05-07 11:09:01 +0200

    arst

--- keymap.c
+++ keymap.c
@@ -21,6 +21,7 @@ enum layers {
 
 enum macros {
   _BSPC,
+  CTL_BSPC,
   _DOWN,
   _ENT,
   _EQL,
@@ -36,10 +37,9 @@ enum macros {
   _SLSH,
   _UP,
   ALT_TAB,
-  END__S__R,
-  ENT__T__N,
-  ESC__D__ALT_F3,
-  HOME__W,
+  _END,
+  _ESC,
+  _HOME,
   LOGIN,
   RALT_Q,
   RALT_SCLN,
@@ -57,8 +57,7 @@ enum macros {
   SFT__SPC,
   SPC__BROWSER,
   VOLDOWN__GUI_LEFT,
-  VOLUP__GUI_RIGHT,
-  END_SFT_HOME_DEL
+  VOLUP__GUI_RIGHT
 };
 
 #define _______ KC_TRNS
@@ -67,10 +66,10 @@ static uint16_t key_timer;
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [COLEMAK] = KEYMAP( \
-    M(SPC__BROWSER), CM_G, CM_D, CM_B, M(RALT_SCLN), RESET, M(LOGIN), _______, CM_COMM, M(SFT__RIGHT_LAYERS), CM_DOT, TG(NUMBERS), \
+    M(SPC__BROWSER), CM_G, CM_D, CM_B, M(RALT_SCLN), RESET, M(LOGIN), _______, CM_COMM, M(SFT__RIGHT_LAYERS), CM_DOT, _______, \
     CM_Q, CM_W, CM_F, CM_P, M(RALT_W), _______, _______, _______, CM_L, CM_U, CM_Y, CM_SCLN, \
     CM_A, CM_R, CM_S, CM_T, M(RALT_Q), _______, _______, _______, CM_N, CM_E, CM_I, CM_O, \
-    CM_Z, CM_X, CM_C, CM_V, M(SFT__LEFT_LAYERS), TG(CAPSLOCK), M(END_SFT_HOME_DEL), KC_SPC, CM_H, CM_K, CM_M, CM_J \
+    CM_Z, CM_X, CM_C, CM_V, M(SFT__LEFT_LAYERS), TG(CAPSLOCK), TG(NUMBERS), KC_SPC, CM_H, CM_K, CM_M, CM_J \
   ),
 
   [CAPSLOCK] = KEYMAP( \
@@ -81,10 +80,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [LEFT_LAYERS] = KEYMAP( \
-    _______, M(ENT__T__N), M(CTL__LEFT_MODS), M(ESC__D__ALT_F3), _______, _______, _______, _______, _______, _______, _______, _______, \
-    _______, M(HOME__W), M(_UP), M(END__S__R), _______, _______, _______, _______, _______, _______, _______, _______, \
+    _______, M(_ENT), M(CTL__LEFT_MODS), M(_ESC), _______, _______, _______, _______, _______, _______, _______, _______, \
+    _______, M(_HOME), M(_UP), M(_END), _______, _______, _______, _______, _______, _______, _______, _______, \
     M(SET_FUNCTION), M(_LEFT), M(_DOWN), M(_RIGHT), _______, _______, _______, _______, _______, _______, _______, _______, \
-    M(SET_NUMBERS), M(SET_DEVELOPMENT), _______, M(_BSPC), _______, _______, _______, _______, _______, _______, _______, _______ \
+    M(SET_NUMBERS), M(SET_DEVELOPMENT), M(CTL_BSPC), M(_BSPC), _______, _______, _______, _______, _______, _______, _______, _______ \
   ),
 
   [LEFT_MODS] = KEYMAP( \
@@ -109,24 +108,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [FUNCTION] = KEYMAP( \
-    _______, _______, KC_TAB, KC_DEL, _______, _______, _______, _______, KC_F10, KC_F11, KC_F12, _______, \
-    _______, LCTL(KC_PGUP), LCTL(KC_UP), LCTL(KC_PGDOWN), _______, _______, _______, _______, KC_F7, KC_F8, KC_F9, _______, \
-    M(SET_FUNCTION), LCTL(KC_LEFT), LCTL(KC_DOWN), LCTL(KC_RIGHT), _______, _______, _______, _______, KC_F4, KC_F5, KC_F6, _______, \
-    _______, _______, _______, KC_BTN3, _______, _______, _______, _______, KC_F1, KC_F2, KC_F3, _______ \
+    _______, KC_PGUP, KC_DEL, KC_PGDOWN, _______, _______, _______, _______, KC_F10, KC_F11, KC_F12, _______, \
+    _______, LCTL(KC_PGUP), KC_TAB, LCTL(KC_PGDOWN), _______, _______, _______, _______, KC_F7, KC_F8, KC_F9, _______, \
+    M(SET_FUNCTION), KC_LSFT, KC_LALT, KC_LCTL, _______, _______, _______, _______, KC_F4, KC_F5, KC_F6, _______, \
+    _______, _______, _______, KC_BTN3, _______, KC_RALT, _______, _______, KC_F1, KC_F2, KC_F3, _______ \
   ),
 
   [NUMBERS] = KEYMAP( \
     _______, _______, _______, _______, _______, _______, _______, _______, KC_7, KC_8, KC_9, _______, \
     _______, _______, KC_LEFT, M(ALT_TAB), _______, _______, _______, _______, KC_4, KC_5, KC_6, _______, \
-    _______, M(VOLDOWN__GUI_LEFT), KC__MUTE, M(VOLUP__GUI_RIGHT), _______, _______, _______, _______, KC_1, KC_2, KC_3, KC_BSPC, \
-    M(SET_NUMBERS), LCTL(CM_Z), LCTL(S(CM_Z)), KC_LSFT, _______, _______, _______, _______, KC_0, KC_DOT, _______, KC_ENT \
+    _______, LCTL(CM_X), LCTL(CM_C), LCTL(CM_V), _______, _______, _______, _______, KC_1, KC_2, KC_3, KC_BSPC, \
+    M(SET_NUMBERS), KC_LSFT, KC_LALT, KC_LCTL, _______, KC_RALT, _______, _______, KC_0, KC_DOT, _______, KC_ENT \
   ),
 
   [DEVELOPMENT] = KEYMAP( \
-    _______, _______, _______, _______, _______, _______, _______, _______, KC_PGUP, KC_GRV, KC_PGDOWN, _______, \
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_GRV, _______, _______, \
     _______, _______, _______, _______, _______, _______, _______, _______, KC_LPRN, KC_RPRN, S(KC_1), _______, \
-    _______, _______, LCTL(CM_C), LCTL(CM_V), _______, _______, _______, _______, S(KC_LBRC), S(KC_RBRC), S(KC_BSLS), _______, \
-    _______, M(SET_DEVELOPMENT), LCTL(CM_X), LCTL(KC_BSPC), _______, _______, _______, _______, KC_LBRC, KC_RBRC, KC_BSLS, _______ \
+    _______, _______, LCTL(CM_S), _______, _______, _______, _______, _______, S(KC_LBRC), S(KC_RBRC), S(KC_BSLS), _______, \
+    _______, M(SET_DEVELOPMENT), _______, _______, _______, _______, _______, _______, KC_LBRC, KC_RBRC, KC_BSLS, _______ \
   ),
 
   [BROWSER] = KEYMAP( \
@@ -219,25 +218,13 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
       }
       break;
     }
-    case ENT__T__N: {
+    case _ENT: {
       if (record->event.pressed) {
         key_timer = timer_read();
         unregister_code(KC_LSFT);
       } else {
-        if (timer_elapsed(key_timer) < QUICK) {
-          register_code(KC_ENT);
-          unregister_code(KC_ENT);
-        } else if (timer_elapsed(key_timer) < SLOW) {
-          register_code(KC_LCTL);
-          register_code(CM_T);
-          unregister_code(CM_T);
-          unregister_code(KC_LCTL);
-        } else {
-          register_code(KC_LCTL);
-          register_code(CM_N);
-          unregister_code(CM_N);
-          unregister_code(KC_LCTL);
-        }
+        register_code(KC_ENT);
+        unregister_code(KC_ENT);
       }
       break;
     }
@@ -268,6 +255,7 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
         unregister_code(KC_LSFT);
         layer_on(FUNCTION);
       } else {
+        clear_mods();
         layer_off(FUNCTION);
       }
       break;
@@ -277,6 +265,7 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
         unregister_code(KC_LSFT);
         layer_on(NUMBERS);
       } else {
+        clear_mods();
         layer_off(NUMBERS);
       }
       break;
@@ -286,6 +275,7 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
         unregister_code(KC_LSFT);
         layer_on(DEVELOPMENT);
       } else {
+        clear_mods();
         layer_off(DEVELOPMENT);
       }
       break;
@@ -295,7 +285,7 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
         layer_on(LEFT_MODS);
         register_code(KC_LCTL);
       } else {
-        unregister_code(KC_LCTL);
+        clear_mods();
         layer_off(LEFT_MODS);
       }
       break;
@@ -306,7 +296,7 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
         layer_on(RIGHT_MODS);
         register_code(KC_LCTL);
       } else {
-        unregister_code(KC_LCTL);
+        clear_mods();
         layer_off(RIGHT_MODS);
       }
       break;
@@ -347,64 +337,33 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
       }
       break;
     }
-    case HOME__W: {
+    case _HOME: {
       if (record->event.pressed) {
         key_timer = timer_read();
         unregister_code(KC_LSFT);
       } else {
-        if (timer_elapsed(key_timer) < NORMAL) {
-          register_code(KC_HOME);
-          unregister_code(KC_HOME);
-        } else {
-          register_code(KC_LCTL);
-          register_code(CM_W);
-          unregister_code(CM_W);
-          unregister_code(KC_LCTL);
-        }
+        register_code(KC_HOME);
+        unregister_code(KC_HOME);
       }
       break;
     }
-    case END__S__R: {
+    case _END: {
       if (record->event.pressed) {
         key_timer = timer_read();
         unregister_code(KC_LSFT);
       } else {
-        if (timer_elapsed(key_timer) < QUICK) {
-          register_code(KC_END);
-          unregister_code(KC_END);
-        } else if (timer_elapsed(key_timer) < SLOW) {
-          register_code(KC_LCTL);
-          register_code(CM_S);
-          unregister_code(CM_S);
-          unregister_code(KC_LCTL);
-        } else {
-          register_code(KC_LCTL);
-          register_code(CM_R);
-          unregister_code(CM_R);
-          unregister_code(KC_LCTL);
-        }
+        register_code(KC_END);
+        unregister_code(KC_END);
       }
       break;
     }
-    case ESC__D__ALT_F3: {
+    case _ESC: {
       if (record->event.pressed) {
         key_timer = timer_read();
         unregister_code(KC_LSFT);
       } else {
-        if (timer_elapsed(key_timer) < QUICK) {
-          register_code(KC_ESC);
-          unregister_code(KC_ESC);
-        } else if (timer_elapsed(key_timer) < SLOW) {
-          register_code(KC_LCTL);
-          register_code(CM_D);
-          unregister_code(CM_D);
-          unregister_code(KC_LCTL);
-        } else {
-          register_code(KC_LALT);
-          register_code(KC_F3);
-          unregister_code(KC_F3);
-          unregister_code(KC_LALT);
-        }
+        register_code(KC_ESC);
+        unregister_code(KC_ESC);
       }
       break;
     }
@@ -493,6 +452,16 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
       }
       break;
     }
+    case CTL_BSPC: {
+      if (record->event.pressed) {
+        unregister_code(KC_LSFT);
+        register_code(KC_LCTL);
+        register_code(KC_BSPC);
+        unregister_code(KC_BSPC);
+        unregister_code(KC_LCTL);
+      }
+      break;
+    }
     case _RALT: {
       if (record->event.pressed) {
         unregister_code(KC_LCTL);
@@ -538,15 +507,6 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
       }
       break;
     }
-    case _ENT: {
-      if (record->event.pressed) {
-        unregister_code(KC_LSFT);
-        register_code(KC_ENT);
-      } else {
-        unregister_code(KC_ENT);
-      }
-      break;
-    }
     case SPC__BROWSER: {
       if (record->event.pressed) {
         key_timer = timer_read();
@@ -556,20 +516,8 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
           register_code(KC_SPC);
           unregister_code(KC_SPC);
         }
+        clear_mods();
         layer_off(BROWSER);
-      }
-      break;
-    }
-    case END_SFT_HOME_DEL: {
-      if (record->event.pressed) {
-        register_code(KC_END);
-        unregister_code(KC_END);
-        register_code(KC_LSFT);
-        register_code(KC_HOME);
-        unregister_code(KC_HOME);
-        unregister_code(KC_LSFT);
-        register_code(KC_DEL);
-        unregister_code(KC_DEL);
       }
       break;
     }
